1e389fd80f336fca9088cfbb672749277256caa7
Hiral <hiral@phdlabs.com>
2018-07-23 14:47:36 +0530

    Load list init

--- src/app/pages/load-list/load-list.component.ts
+++ src/app/pages/load-list/load-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-load-list',
   templateUrl: './load-list.component.html',
-  styleUrls: ['./load-list.component.css']
+  styleUrls: ['./load-list.component.css'],
 })
 export class LoadListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {    
   }
 
 }
